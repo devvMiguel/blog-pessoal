8e399e809bfd7c3f565146ab0b50962f4c0c5082
devvMiguel <miguelgalvao_galvao@hotmail.com>
2025-07-03 11:56:37 -0300

    feat: implementação de camada de segurança utilizando a lib: passport

--- src/postagem/postagem.module.ts
+++ src/postagem/postagem.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TemaModule } from '../tema/tema.module';
 import { Postagem } from './entities/postagem.entity';
 import { PostagemService } from './services/postagem.service';
 import { PostagemController } from './controllers/postagem.controller';
-import { TemaModule } from '../tema/tema.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Postagem]), TemaModule],
---
+++ src/usuario/services/usuario.service.ts
@@ -0,0 +1,71 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Bcrypt } from '../../auth/bcrypt/bcrypt';
+import { Usuario } from '../entities/usuario.entity';
+
+@Injectable()
+export class UsuarioService {
+  constructor(
+    @InjectRepository(Usuario)
+    private usuarioRepository: Repository<Usuario>,
+    private bcrypt: Bcrypt,
+  ) {}
+
+  async findAll(): Promise<Usuario[]> {
+    return await this.usuarioRepository.find();
+  }
+
+  async findByUsuario(usuario: string): Promise<Usuario | null> {
+    return await this.usuarioRepository.findOne({
+      where: {
+        usuario: usuario,
+      },
+    });
+  }
+
+  async findById(id: number): Promise<Usuario> {
+    const usuario = await this.usuarioRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!usuario) {
+      throw new HttpException(
+        'Esse usuário não achei! :/',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    return usuario;
+  }
+
+  async create(usuario: Usuario): Promise<Usuario> {
+    const buscaUsuario = await this.findByUsuario(usuario.usuario);
+
+    if (buscaUsuario) {
+      throw new HttpException(
+        'Esse Usuario já existe! :/',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    usuario.senha = await this.bcrypt.criptografarSenha(usuario.senha);
+    return await this.usuarioRepository.save(usuario);
+  }
+
+  async update(usuario: Usuario): Promise<Usuario> {
+    await this.findById(usuario.id);
+
+    const buscaUsuario = await this.findByUsuario(usuario.usuario);
+
+    if (buscaUsuario && buscaUsuario.id !== usuario.id) {
+      throw new HttpException(
+        'Usuário (e-mail) já Cadastrado! :/',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    usuario.senha = await this.bcrypt.criptografarSenha(usuario.senha);
+    return await this.usuarioRepository.save(usuario);
+  }
+}
